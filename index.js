3d7ae31e8881116ab87227f90bc25b8885ee5db7
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-25 23:07:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers(drivers, returnFirstTwoDrivers
-  
-}
+function selectDifferentDrivers(drivers, fn) {
+  return fn(drivers);
+};
